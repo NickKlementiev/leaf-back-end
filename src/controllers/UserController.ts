a6a28f1695058a719808bd62b8fba63e6e0543a3
NickKlementiev <nklementiev@hotmail.com>
2021-03-14 16:06:17 -0300

    :sparkles: Added controller's functionalities

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
-        return response.send();
+        const { name, password } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            name,
+        });
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: 'User already exists!',
+            });
+        }
+
+        const user = usersRepository.create({
+            name,
+            password,
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
