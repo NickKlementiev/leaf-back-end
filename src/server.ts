a0506e9ac211074442b562abf0ee45f79862fa9e
NickKlementiev <nklementiev@hotmail.com>
2021-03-14 11:47:59 -0300

    :sparkles: Hello, World!

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"));
+app.get("/users", (request, response) => {
+    return response.send("<h1>Hello, world!</h1>");
+});
+
+app.listen(3333, () => console.log('Server is running!'));
