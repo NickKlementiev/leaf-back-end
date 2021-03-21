cf056a1499e1ef49ddd724546d1079324c643022
NickKlementiev <nklementiev@hotmail.com>
2021-03-21 15:36:46 -0300

    :sparkles: Added authentication controller

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
+
+class LoginController {
+    async authenticate(request: Request, response: Response) {
+        const { username, password } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const auth = await usersRepository.findOne({
+            username,
+            password,
+        });
+
+        if (!auth) {
+            return response.status(400).send({
+                error: 'Authentication failed!',
+            });
+        }
+
+        if (!request.session['username']) {
+            request.session['username'] = username;
+            request.session['name'] = auth.name;
+            request.session['password'] = password;
+            request.session.save();
+        }
+
+        return response.status(201).json({
+            message: "Authentication succesful!",
+        });
+    }
+
+    async logout(request: Request, response: Response) {
+        if (!request.session['name']) {
+            return response.status(401).json({
+                error: "Already logged out!"
+            });
+        }
+
+        request.session.destroy(() => {
+            return response.status(200).json({
+                message: "Logout succesful!"
+            });
+        });
+    }
+
+    async login(request: Request, response: Response) {
+        if (!request.session['username']) {
+            return response.status(401).json({
+                error: "Login failed!"
+            });
+        }
+
+        request.session.reload(() => {
+            console.log("Session reloaded!");
+        });
+
+        return response.status(200)
+            .json({
+                message: "Welcome, " + request.session['name'] + "!"
+            });
+    }
+}
+
+export { LoginController };
