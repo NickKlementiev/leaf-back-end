8720f06d66a0e204da30d17d3223e84649a8eafc
NickKlementiev <nklementiev@hotmail.com>
2021-03-21 15:39:10 -0300

    :sparkles: Added basic session configuration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import 'reflect-metadata';
 import express from 'express';
+import session from 'express-session';
 import { router } from './routes';
-
+import { v4 as uuid } from 'uuid';
 import './database';
 
 const app = express();
+const sess = {
+    genid: () => {
+        return uuid();
+    },
+    secret: 'Keep it secret',
+    name: 'uniqueSessionId',
+    saveUninitialized: false,
+    resave: false,
+};
 
 app.use(express.json());
+app.use(session(sess));
 app.use(router);
 
 export { app };
