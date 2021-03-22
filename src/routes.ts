2bff968849e6239372e4a87ad1be36eded7f253a
NickKlementiev <nklementiev@hotmail.com>
2021-03-22 13:28:20 -0300

    :sparkles: Added organization http methods

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { LoginController } from './controllers/LoginController';
+import {OrganizationController} from './controllers/OrganizationController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
 const loginController = new LoginController();
+const orgController = new OrganizationController();
 
 router.get('/users', userController.show);
 router.post('/users', userController.create);
@@ -15,5 +17,10 @@ router.delete('/users/:id', userController.delete);
 router.get('/login', loginController.login);
 router.post('/authenticate', loginController.authenticate);
 router.get('/logout', loginController.logout);
+
+router.get('/organizations', orgController.show);
+router.post('/organizations', orgController.create);
+router.put('/organizations/:id', orgController.update);
+router.delete('/organizations/:id', orgController.delete);
 
 export { router };
