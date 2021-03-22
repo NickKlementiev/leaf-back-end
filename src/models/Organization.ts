f7d4e7ebddbb1fb58df8f49aff4426ed16df8d2c
NickKlementiev <nklementiev@hotmail.com>
2021-03-22 13:24:50 -0300

    :sparkles: Added organizations table and entity

---
+++ src/models/Organization.ts
@@ -0,0 +1,15 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm";
+
+@Entity('organizations')
+class Organization {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+}
+
+export {Organization}
