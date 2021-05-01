0af02bd8c7d29d1efb1314ada16ee0adde300a39
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-05-01 18:58:02 -0300

    ✨ Adding socket methods

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,51 @@
 import { app } from './app';
 import { Server } from 'http';
-import { Server as SocketServer, Socket } from 'socket.io';
+
 import { getManager } from 'typeorm';
+import { Socket } from 'socket.io';
 
 const httpServer = new Server(app);
-const io = new SocketServer(httpServer);
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
 
 async function getMessages() {
-    const entityManager = getManager();
-    const messages = entityManager.query(`SELECT U.name as sender, M.content
+  const entityManager = getManager();
+  const messages = entityManager.query(`SELECT U.name as sender, M.content
                                          FROM users AS U INNER JOIN messages AS M
                                          ON M.sender = U.id`);
-    return messages;
+  return messages;
 }
 
+io.use((socket, next) => {
+  const name = socket.handshake.auth.name;
+  socket.name = name;
+  next();
+});
+
 io.on('connection', (socket: Socket) => {
-    console.log(`Socket connected, ID: ${socket.id}`);
+  console.log(`Socket connected, ID: ${socket.id}`);
+  io.sockets.sockets['name'] = socket.name;
+  io.sockets.sockets['id'] = socket.id;
 
-    getMessages().then((messages) => socket.emit('previousMessages', messages));
+  const users = [];
 
-    socket.on('sendMessage', (data) => {
-        socket.broadcast.emit('receivedMessage', data);
+  io.sockets.sockets.forEach((socket) => {
+    users.push({
+      userID: socket.id,
+      name: socket.name,
     });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected!');
-    });
+  socket.emit('users', users);
+
+  socket.broadcast.emit('userConnected', {
+    userID: socket.id,
+    name: socket.name,
+  });
+  getMessages().then((messages) => socket.emit('previousMessages', messages));
 });
 
 httpServer.listen(3333, () => console.log('Leaf-Back-End is running!'));
