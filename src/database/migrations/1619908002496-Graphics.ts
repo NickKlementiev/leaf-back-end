51ad3849e974d2f3d71b17e71e5c7256cd3fe3c0
NickKlementiev <nklementiev@hotmail.com>
2021-05-01 21:04:39 -0300

    :sparkles: Added graphics and data tables on DB

---
+++ src/database/migrations/1619908002496-Graphics.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Graphics1619908002496 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'graphics',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'type',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('graphics');
+    }
+}
